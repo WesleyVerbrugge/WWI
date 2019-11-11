67fd7d9a8c07099cec8c19d780a6e72944ce8462
EmstallZ <EmstallZ@users.noreply.github.com>
2019-11-11 13:42:01 +0100

    initial push
    basis voor het inlogpagina en registreer pagina

---
+++ login/registreer.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="post">
+    <label>Voornaam:
+        <input type="text" name="voornaam" required>*
+    </label><br>
+    <label>Tussenvoegsel:
+        <input type="text" name="tussenvoegsel">
+    </label><br>
+    <label>Achternaam:
+        <input type="text" name="achternaam" required>*
+    </label><br>
+    <label>Email:
+        <input type="email" name="email" required>*
+    </label><br>
+    <label>Wachtwoord:
+        <input type="password" name="wachtwoord" required>*
+    </label><br>
+    <label>Telefoonnummer:
+        <input type="tel" name="telefoonnummer" required>*
+    </label><br>
+<br>
+    <label>Land:
+        <input type="text" name="land" required>*
+    </label><br>
+    <label>Woonplaats:
+        <input type="text" name="woonplaats" required>*
+    </label><br>
+    <label> Straatnaam:
+        <input type="text" name="adress" required>*
+    </label>
+    <label> Nummer:
+        <input type="number" name="straatnummer" required>*
+    </label><br>
+    <label> Secundair Straatnaam:
+        <input type="text" name="adress2">
+    </label>
+    <label> Nummer:
+        <input type="number" name="straatnummer2">
+    </label><br>
+    <label>Postcode:
+        <input type="text" name="postcode" required>*
+    </label><br>
+
+
+
+         <input type="submit">
+
+</form>
+
+
+</body>
+</html>
