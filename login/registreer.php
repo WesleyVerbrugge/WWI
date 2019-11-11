b9523c367e603ddcf9b8f270b868500a4e1df52c
EmstallZ <EmstallZ@users.noreply.github.com>
2019-11-11 15:17:31 +0100

    Update registreer.php
    div aangepast/toegevoegd

--- login/registreer.php
+++ login/registreer.php
@@ -10,50 +10,51 @@
 <body>
 
 <form method="post">
-    <label>Voornaam:
-        <input type="text" name="voornaam" required>*
-    </label><br>
-    <label>Tussenvoegsel:
-        <input type="text" name="tussenvoegsel">
-    </label><br>
-    <label>Achternaam:
-        <input type="text" name="achternaam" required>*
-    </label><br>
-    <label>Email:
-        <input type="email" name="email" required>*
-    </label><br>
-    <label>Wachtwoord:
-        <input type="password" name="wachtwoord" required>*
-    </label><br>
-    <label>Telefoonnummer:
-        <input type="tel" name="telefoonnummer" required>*
-    </label><br>
-<br>
-    <label>Land:
-        <input type="text" name="land" required>*
-    </label><br>
-    <label>Woonplaats:
-        <input type="text" name="woonplaats" required>*
-    </label><br>
-    <label> Straatnaam:
-        <input type="text" name="adress" required>*
-    </label>
-    <label> Nummer:
-        <input type="number" name="straatnummer" required>*
-    </label><br>
-    <label> Secundair Straatnaam:
-        <input type="text" name="adress2">
-    </label>
-    <label> Nummer:
-        <input type="number" name="straatnummer2">
-    </label><br>
-    <label>Postcode:
-        <input type="text" name="postcode" required>*
-    </label><br>
 
+    <div>
+        <label>Voornaam:
+            <input type="text" name="voornaam" required>*
+        </label><br>
+        <label>Tussenvoegsel:
+            <input type="text" name="tussenvoegsel">
+        </label><br>
+        <label>Achternaam:
+            <input type="text" name="achternaam" required>*
+        </label><br>
+        <label>Email:
+            <input type="email" name="email" required>*
+        </label><br>
+        <label>Wachtwoord:
+            <input type="password" name="wachtwoord" required>*
+        </label><br>
+        <label>Telefoonnummer:
+            <input type="tel" name="telefoonnummer" required>*
+        </label><br>
+        <br>
+        <label>Land:
+            <input type="text" name="land" required>*
+        </label><br>
+        <label>Woonplaats:
+            <input type="text" name="woonplaats" required>*
+        </label><br>
+        <label> Straatnaam:
+            <input type="text" name="adress" required>*
+        </label>
+        <label> Nummer:
+            <input type="number" name="straatnummer" required>*
+        </label><br>
+        <label> Secundair Straatnaam:
+            <input type="text" name="adress2">
+        </label>
+        <label> Nummer:
+            <input type="number" name="straatnummer2">
+        </label><br>
+        <label>Postcode:
+            <input type="text" name="postcode" required>*
+        </label><br>
+    </div>
 
-
-         <input type="submit">
+    <input type="submit">
 
 </form>
 
