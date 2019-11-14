55a06da0568763aed2b4d44ba0a8e856a9a09d19
EmstallZ <EmstallZ@users.noreply.github.com>
2019-11-14 23:54:33 +0100

    fucnties voor controleren gegevens
    functies gemaakt om gegevens te controleren, functies zijn nog niet helemaal af. ze werken wel maar doen nog niks

--- login/registreer.php
+++ login/registreer.php
@@ -1,21 +1,18 @@
 <?php
-include "functions.php";
+require_once "functions.php";
 
 try {
     $host = "127.0.0.1";
     $databasename = "wideworldimporters";
     $user = "root";
-    $pass ="";
+    $pass = "";
     $port = 3306;
     $connection = mysqli_connect($host, $user, $pass, $databasename, $port);
-
-    $result = mysqli_query($connection, "SELECT countryname FROM countries");
 
 
 } catch (Exception $e) {
     print ("An error has occurred");
 }
-
 
 
 ?>
@@ -34,6 +31,89 @@ try {
 <body>
 
 <?php
+function check_given_value_name($name){
+
+    if ((isset($_POST[$name]) && !empty($_POST[$name]) && isset($_POST["submit"]))) {
+        $maxlengte = 50;
+        //gebruik textoutput na de volgende ifstatement
+        $textoutput = trim($_POST[$name]);
+        $textoutput = stripslashes($textoutput);
+        if (strlen($textoutput) <= $maxlengte){
+            //todo do something
+        } else {
+            print ("Je " . $name . " mag maar maximaal " . $maxlengte . " tekens lang zijn.<br>");
+        }
+    } else {
+        print ("Je moet verplicht een " . $name . " invoeren. <br>");
+    }
+
+}
+
+function check_given_value_achtername($name){
+
+    if ((isset($_POST[$name]) && !empty($_POST[$name]) && isset($_POST["submit"]))) {
+        $maxlengte = 50;
+        $tussenvoegsel = "";
+        if (isset($_POST["tussenvoegsel"]) && !empty($_POST["tussenvoegsel"])){
+            $tussenvoegsel = $_POST["tussenvoegsel"];
+        }
+        //gebruik textoutput na de volgende ifstatement
+        $achternaam = $_POST[$name];
+        $achternaam = $tussenvoegsel . " " . $achternaam;
+        $textoutput = trim($achternaam);
+        $textoutput = stripslashes($textoutput);
+        if (strlen($textoutput) <= $maxlengte){
+            //todo do something
+        } else {
+            print ("Je " . $name . " mag maar maximaal " . $maxlengte . " tekens lang zijn.<br>");
+        }
+    } else {
+        print ("Je moet verplicht een " . $name . " invoeren. <br>");
+    }
+
+}
+
+function check_given_value_email($email){
+
+    if ((isset($_POST[$email]) && !empty($_POST[$email]) && isset($_POST["submit"]))) {
+        $maxlengte = 256;
+        //gebruik textoutput na de volgende ifstatement
+        $textoutput = trim($_POST[$email]);
+        $textoutput = stripslashes($textoutput);
+        if (strlen($textoutput) <= $maxlengte){
+            if (filter_var($textoutput, FILTER_VALIDATE_EMAIL)){
+            //todo do something
+            } else {
+                print ("Het opgegeven email is niet geldig.<br>");
+            }
+        } else {
+            print ("Je " . $email . " adres mag maar maximaal " . $maxlengte . " tekens lang zijn.<br>");
+        }
+    } else {
+        print ("Je moet verplicht een " . $email . " invoeren.<br>");
+    }
+
+}
+
+function check_password($wachtwoord, $wachtwoord2){
+
+    if (isset($_POST[$wachtwoord]) && !empty($_POST[$wachtwoord])){
+        if (isset($_POST[$wachtwoord2]) && !empty($_POST[$wachtwoord2])){
+
+        } else {
+            print ("Je moet verplicht een tweede wachtoord invoeren.");
+        }
+    } else {
+        print ("Je moet verplicht een wachtoord invoeren.");
+    }
+}
+
+//validation codes
+check_given_value_name("voornaam");
+check_given_value_achtername("achternaam");
+check_given_value_email("email");
+check_password("wachtwoord", "wachtwoord2");
+
 
 ?>
 
@@ -69,84 +149,19 @@ try {
                 <input type="password" name="wachtwoord2" required>*
             </label><br>
         </div>
-        <div>
-            <label>Telefoonnummer:
-                <input type="tel" name="telefoonnummer" <?php isset_value("telefoonnummer"); ?> required>*
-            </label><br>
-        </div>
-        <br>
-        <div>
-            <label>Land:
-                <select name="land">
-                    <?php
-
-                    if (isset($_POST["land"])){
-                        $givencountry = $_POST["land"];
-                    }
-
-                    $landenarray = array();
-
-                    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                        $country = $row["countryname"];
-                        $landenarray[] = $row["countryname"];
-
-                        $selectedcountry = "";
-
-                        if (in_array($givencountry,$landenarray)){
-                            if ($row["countryname"] == $givencountry){
-                                $selectedcountry = "selected";
-                            }
-                        }
-                        echo ("<option value='" . $country . "' " . $selectedcountry . ">" . $country . "</option>");
-                    }
-
-                    ?>
-                </select>
-            </label><br>
-        </div>
-        <div>
-            <label>Woonplaats:
-                <input type="text" name="woonplaats" <?php isset_value("woonplaats"); ?> required>*
-            </label><br>
-        </div>
-        <div>
-            <label> Straatnaam:
-                <input type="text" name="adress" <?php isset_value("adress"); ?> required>*
-            </label>
-        </div>
-        <div>
-            <label> Nummer:
-                <input type="number" name="straatnummer" <?php isset_value("straatnummer"); ?> required>*
-            </label><br>
-        </div>
-        <div>
-            <label> Secundair Straatnaam:
-                <input type="text" name="adress2" <?php isset_value("adress2"); ?>>
-            </label>
-        </div>
-        <div>
-            <label> Nummer:
-                <input type="number" name="straatnummer2" <?php isset_value("straatnummer2"); ?>>
-            </label><br>
-        </div>
-        <div>
-            <label>Postcode:
-                <input type="text" name="postcode" <?php isset_value("postcode"); ?> required>*
-            </label><br>
-        </div>
     </div>
-
-    <input type="submit">
+    <input type="submit" name="submit" value="verzonden">
 </form>
 
 
+<pre>
 <?php
 
 
 mysqli_close($connection);
 
 ?>
-
+</pre>
 
 </body>
 </html>
