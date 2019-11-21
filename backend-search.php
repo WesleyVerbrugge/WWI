931299504a8421d0f1a5f85a9ccd783cfcb58e98
EmstallZ <EmstallZ@users.noreply.github.com>
2019-11-21 10:10:52 +0100

    categorien toegevoegd

--- backend-search.php
+++ backend-search.php
@@ -5,18 +5,26 @@ server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "", "wideworldimporters");
 
 // Check connection
-if($link === false){
+if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
- 
-if(isset($_GET["Term"]) && isset($_GET["Schoice"])){
+
+if (isset($_GET["Term"]) && isset($_GET["Schoice"])) {
     // Prepare a select statement
-    if(isset($_GET["Schoice"])) {
-      if($_GET["Schoice"] == "aname") {
-        $sql = "SELECT * FROM stockitems LEFT JOIN images_stockitems ON images_stockitems.StockItemID = stockitems.StockItemID WHERE StockItemName LIKE ?";
-      } else {
-        $sql = "SELECT * FROM stockitems LEFT JOIN images_stockitems ON images_stockitems.StockItemID = stockitems.StockItemID where stockitems.stockitemID like ?";
-      }
+    if (isset($_GET["Schoice"])) {
+        if ($_GET["Schoice"] == "aname") {
+            $sql = "SELECT * FROM stockitems LEFT JOIN images_stockitems ON images_stockitems.StockItemID = stockitems.StockItemID WHERE StockItemName LIKE ?";
+        } else {
+            $sql = "SELECT * FROM stockitems LEFT JOIN images_stockitems ON images_stockitems.StockItemID = stockitems.StockItemID where stockitems.stockitemID like ?";
+        }
+    }
+
+    //sql kwiery voor categorien
+    $sql_categorien = "SELECT s.StockGroupName, count(*) as AantalProductCategorie FROM stockgroups s JOIN stockitemstockgroups g ON s.StockGroupID=g.StockGroupID JOIN stockitems i ON g.StockItemID=i.StockItemID JOIN stockitemholdings h ON i.StockItemID=h.StockItemID group by s.StockGroupName";
+    $result_categorien = mysqli_query($link, $sql_categorien);
+    $arr_categorien = array();
+    while ($row = mysqli_fetch_array($result_categorien, MYSQLI_ASSOC)) {
+        $arr_categorien[$row["StockGroupName"]] = $row["AantalProductCategorie"];
     }
 
     //sql kwiery voor het aantal producten
@@ -34,49 +42,49 @@ if(isset($_GET["Term"]) && isset($_GET["Schoice"])){
     }
 
 
-    if($stmt = mysqli_prepare($link, $sql)){
+    if ($stmt = mysqli_prepare($link, $sql)) {
         // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "s", $param_term);
-        
+
         // Set parameters
-        if(isset($_GET["Schoice"])) {
-          if($_GET["Schoice"] == "aname") {
-            $param_term = '%' . $_GET["Term"] . '%';
-          } else {
-            $param_term = $_GET["Term"];
-          }
+        if (isset($_GET["Schoice"])) {
+            if ($_GET["Schoice"] == "aname") {
+                $param_term = '%' . $_GET["Term"] . '%';
+            } else {
+                $param_term = $_GET["Term"];
+            }
         }
-        
-        
+
+
         // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
+        if (mysqli_stmt_execute($stmt)) {
             $result = mysqli_stmt_get_result($stmt);
             // Check number of rows in the result set
-            if(mysqli_num_rows($result) > 0){
+            if (mysqli_num_rows($result) > 0) {
                 // Fetch result rows as an associative array
                 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            } else{
+            } else {
                 echo "<p>No matches found</p>";
             }
-        } else{
+        } else {
             echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
         }
     }
-     
+
     // Close statement
     mysqli_stmt_close($stmt);
 }
- 
+
 // close connection
 mysqli_close($link);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
 <!--<table style="border: solid;">-->
@@ -105,38 +113,58 @@ mysqli_close($link);
 <!--</tbody>-->
 <!--</table>-->
 <div class="container-fluid" id="container-producten">
+    <div class="sticky-top">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <?php
+                foreach ($arr_categorien as $categorie => $aantalproductcategorie) {
+                    print '<li class="breadcrumb-item"><a href="#"> '. $categorie . '</a></li>';
+                }
+                ?>
+            </ol>
+        </nav>
+    </div>
     <div class="row d-flex justify-content-center">
-<?php
-if (isset($_GET["azr"])){
-    $azr = $_GET["azr"];
-    $i = 1;
-    if ($azr == "max" || $azr > $aantalproducten || $azr < 1){
-        $azr = $aantalproducten;
-    }
-    if(isset($result)) {
-    foreach ($result as $row){
-        if ($i <= $azr){
-            ?>
-            <div class="card producten" id="producten">
-                <img src="<?php if(empty($row['image'])){ echo "placeholder.png"; } else { echo $row['image']; } ?>" class="card-img-top" alt="Product Image">
-                <div class="card-body card-body-text row" id="card-body">
-                    <h5 class="card-title"><?php echo $row["StockItemName"]; ?></h5>
-                    <div class="card-tekst"><p class="card-text"><?php /*echo $row["SearchDetails"];*/echo $row["MarketingComments"]; ?></p></div>
-                    <div><a href="showitem.php?item_id=<?php echo $row["StockItemID"] ?>" class="btn btn-primary koop-knop">Bekijk Product</a><div class="product-price">&#8364;&nbsp;<?php echo $row["RecommendedRetailPrice"]; ?></div></div>
-                </div>
+        <?php
+        if (isset($_GET["azr"])) {
+            $azr = $_GET["azr"];
+            $i = 1;
+            if ($azr == "max" || $azr > $aantalproducten || $azr < 1) {
+                $azr = $aantalproducten;
+            }
+            if (isset($result)) {
+                foreach ($result as $row) {
+                    if ($i <= $azr) {
+                        ?>
+                        <div class="card producten" id="producten">
+                            <img src="<?php if (empty($row['image'])) {
+                                echo "placeholder.png";
+                            } else {
+                                echo $row['image'];
+                            } ?>" class="card-img-top" alt="Product Image">
+                            <div class="card-body card-body-text row" id="card-body">
+                                <h5 class="card-title"><?php echo $row["StockItemName"]; ?></h5>
+                                <div class="card-tekst"><p class="card-text"><?php /*echo $row["SearchDetails"];*/
+                                        echo $row["MarketingComments"]; ?></p></div>
+                                <div><a href="showitem.php?item_id=<?php echo $row["StockItemID"] ?>"
+                                        class="btn btn-primary koop-knop">Bekijk Product</a>
+                                    <div class="product-price">
+                                        &#8364;&nbsp;<?php echo $row["RecommendedRetailPrice"]; ?></div>
+                                </div>
+                            </div>
 
-            </div>
-<?php
-            $i++;
+                        </div>
+                        <?php
+                        $i++;
+                    }
+                }
+            } else {
+                $defURL = "index.php";
+                header('Location: ' . $defURL);
+            }
         }
-    }
-  } else {
-    $defURL = "index.php";
-    header('Location: '.$defURL);
-  }
-}
-?>
+        ?>
     </div>
-    </div>
+</div>
 </body>
 </html>
