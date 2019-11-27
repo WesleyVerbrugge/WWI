70b2ad5f88ecacf3fd473a7bceae343f786538c4
Wesley Verbrugge <w.verbrugge@home.nl>
2019-11-27 19:32:17 +0100

    kleine fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include_once "header.php"; ?>
 <body>
 <div class="container-fluid">
-    <div class="jumbotron"><h3 class="display-2">Welkom&nbsp; <?php print($_SESSION['user_data']['PreferredName']); ?></h3></div>
+    <div class="jumbotron"><h3 class="display-2">Welkom&nbsp; <?php if(isset($_SESSION['user_data'])){ print($_SESSION['user_data']['PreferredName']);} ?></h3></div>
 </div>
 </body>
 <?php include_once "footer.php"; ?>
