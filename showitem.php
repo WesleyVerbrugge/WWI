28deae4a7224562a46de654093804cefb942ca7c
Wesley <w.verbrugge@home.nl>
2019-12-16 20:49:00 +0100

    review part 2

--- showitem.php
+++ showitem.php
@@ -129,20 +129,19 @@ $sql_kortingPercentage = "SELECT DiscountPercentage FROM specialdeals WHERE Stoc
                 echo "<div class='row'>";
             foreach ($q2 as $review){
                 echo "<div class='col-sm'>";
+                echo '
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-muted">' . $q2->FirstName . $q2->LastName . '</h6>
+                    <p class="card-text">' . $q2->review . '</p>
+                </div>
+                </div>
+                ';
             }
             echo "</div>";
             }
 
             ?>
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div>
-        </div>
         </div>
     </div>
       </div>
