f4634cf839ddc5df9d1a397be24794b0abea8c6f
EmstallZ <EmstallZ@users.noreply.github.com>
2019-12-17 20:06:39 +0100

    Shoppingcart nieuwe functionaliteiten + styling
    hoeveelheid producten kan worden aangepast, product kan worden verwijderd, verder winkelen, doorverwijzing product in winkelmand, alert voor gratis verzending

--- shoppingcart.php
+++ shoppingcart.php
@@ -45,15 +45,16 @@ if (isset($_GET["LeegCart"])){
         } else {
 
     ?>
-
-<table class="shoppingcarttable">
-    <thead>
-        <th>Product</th>
-        <th class="tableTextRight">Prijs Per Stuk</th>
-        <th class="tableTextRight">Aantal</th>
-        <th class="tableTextRight">Verwijderen</th>
-        <th class="tableTextRight">Totaal</th>
-    </thead>
+            <table id="cart" class="table table-hover table-condensed" style="margin-left: 0">
+                <thead>
+                <tr>
+                    <th style="width:50%">Product</th>
+                    <th style="width:10%">Price</th>
+                    <th style="width:8%">Quantity</th>
+                    <th style="width:22%" class="text-center">Subtotal</th>
+                    <th style="width:8%"></th>
+                </tr>
+                </thead>
     <?php
     //sql query product informatie
 $sql = "SELECT * FROM stockitems left JOIN stockitemholdings ON stockitems.StockItemID = stockitemholdings.StockItemID left JOIN images_stockitems ON images_stockitems.StockItemID = stockitems.StockItemID WHERE stockitems.StockItemID = ?";
@@ -82,100 +83,124 @@ for ($i = 0; $i < (count($_SESSION["winkelwagen"])); $i++) {
         $ItemID = $row["StockItemID"];
         $naam = $row["StockItemName"];
         $prijs = $row["RecommendedRetailPrice"];
+        $beschrijving = $row["MarketingComments"];
         ?>
         <tbody>
         <tr>
             <!-- foto van product -->
-            <td class="column1Shoppincart"><img height="75" width="75" src="images/img1.jpg"><?php print ($naam) ?></td>
-            <a class="column2Shoppingcart">
+            <td data-th="Product">
+                <div class="row">
+                    <div class="col-sm-2 hidden-xs"><img height="100px" src="images/img1.jpg" alt="Product Image" class="img-responsive"/></div>
+                    <div class="col-sm-10">
+                        <h4 class="nomargin"><a class="stretched-link text-dark" href="showitem.php?item_id=<?php print($ItemID); ?>"><?php print ($naam); ?></a></h4>
+                        <p><?php if (!empty($beschrijving)){print $beschrijving;} ?></p>
+                    </div>
+                    </div>
+            </td>
+            <td data-th="Price">&#8364;<?php print (str_replace('.', ',', $prijs)) ?></td>
                 <!-- prijs per stuk met een str_replace om een . naar een , te veranderen -->
-            <td class="tableTextRight">&#8364;<?php print (str_replace('.', ',', $prijs)) ?></td>
-                <td class="tableTextRight">
-                    <!-- form om de aantallen te veranderen -->
-                    <?php 
+            <!-- form om de aantallen te veranderen -->
+            <td data-th="Quantity" style="text-align: center">
+                <form method="post">
+                    <div class="quantity">
+                <input style="width: 60%; display: inline-block" min="1" class="form-control" type="number" name="aantalProducten<?php print ($ItemID); ?>" value="<?php
                     foreach($_SESSION['winkelwagen'] as $item) {
-                        // var_dump($item['item_id']);
-                        // var_dump($row['StockItemID']);
                         if($item['item_id'] == $row['StockItemID']){
-                            echo $item['quantity'];
+                            $aantalproductid = "aantalProducten" . $ItemID;
+                            if (isset($_POST[$aantalproductid])){
+                                $_SESSION["winkelwagen"][$i]["quantity"] = $_POST[$aantalproductid];
+                            }
+                            echo $_SESSION["winkelwagen"][$i]["quantity"];
                         }
-                        $aantal = $item['quantity'];
+//                        $aantal = $_SESSION["winkelwagen"][$i]["quantity"];
                     }
-                    ?>
+
+                    ?>"></div>
+                    <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+                </form>
                 </td>
-                    <td class="tableTextRight">
-                        <!-- button voor het verwijderen van een product uit de winkelwagen -->
-                        <form action="shoppingcart.php" method="get">
-                            <button type="submit" class="btn btn-danger" name="verwijderProduct">-</button>
-                            <input type="hidden" name="delete_item_id" value="<?php echo $row['StockItemID']; ?>">
-                        </form>
-                    </td>
-            <td class="tableTextRight">&#8364;<?php
-                // totaal prijs van het product * aantal
-                    $totaal = $prijs * $aantal;
-                    print (str_replace('.', ',', $totaal));
-                    $eindtotaal = $eindtotaal + $totaal;
-                ?></td>
-            </a>
+            <td data-th="Subtotal" class="text-center">&#8364;<?php print(str_replace('.', ',',$totaal = $prijs * $_SESSION["winkelwagen"][$i]["quantity"])); $eindtotaal += $totaal; ?></td>
+
+<td>
+    <form action="shoppingcart.php" method="get">
+        <button type="submit" class="btn btn-danger" name="verwijderProduct"><i class="fa fa-trash-o"></i></button>
+        <input type="hidden" name="delete_item_id" value="<?php echo $row['StockItemID']; ?>">
+    </form>
+</td>
         </tr>
+
       <?php
     }
 
-
-    if ($eindtotaal < 50){
-        $eindtotaal = $eindtotaal + 3.95;
-
-
-        ?>
-    <tr>
-       <td></td>
-        <td></td>
-        <td></td>
-        <td class="tableTextRight">Verzend kosten:</td>
-        <td class="tableTextRight">&euro;3.95</td>
-    </tr>
-
-
-        <?php
-    } else {
-        ?>
-        <tr>
-       <td></td>
-        <td></td>
-        <td></td>
-        <td class="tableTextRight">Verzend kosten:</td>
-        <td class="tableTextRight text-success">Free Shipping</td>
-    </tr>
-        <?php
-    }
-?>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="tableTextRight">Totaal bedrag:</td>
-        <td class="totaalbedrag tableTextRight">&#8364;<?php
-
-            //print het te betalen bedrag voor de gehele winkelwagen
-            print (str_replace('.', ',', $eindtotaal));
-            ?></td>
-    </tr>
+ ?>
+        </tbody>
+                <tfoot>
+                <tr class="visible-xs">
+                    <td></td>
+                <td></td>
+                    <td style="text-align: right"><strong>Shipping:</strong></td>
+                    <td style="text-align: center"><?php $shippingCost = 3.95; if ($eindtotaal < 50){ $freeShippingPrice = str_replace(".",",",50 - $eindtotaal); print ("<strong>&#8364; " . str_replace(".",",",$shippingCost) . "</strong><br><div class=\"alert alert-warning\" style='font-size: small; padding: 0; margin: 0;' role=\"alert\">Add <color class='text-success'>&#8364; $freeShippingPrice</color> worth of products to receive <color class='text-success'><b>free shipping</b></color>!</div>"); $eindtotaal += $shippingCost;} else { print ("<color class='text-success'>Free shipping</color>");}  ?></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><a href="backend-search.php?Term=&Schoice=aname&azr=max" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+                    <td colspan="2" class="hidden-xs"></td>
+                    <td class="hidden-xs text-center"><strong><?php print ("&#8364; " . str_replace(".", ",",$eindtotaal)); ?></strong></td>
+                    <td><a href="orderCheck.php" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
+                </tr>
+                </tfoot>
 </table>
 <p>
-            <!-- button voor het legen van de gehele winkelwagen -->
-    <form class="margin-left" action="shoppingcart.php" method="get">
+            <!-- button voor het legen van de gehele winkelwagen
+            Deze knop is licht zichtbaar voor het debuggen
+            -->
+    <form class="margin-left" action="shoppingcart.php" method="get" style="opacity: 10%">
         <input type="submit" name="LeegCart" value="leeg winkelwagen">
     </form>
 </p>
             <!-- knop die je naar de pagina voor het zien van je bestelling brengt -->
-            <form class="margin-left" action="orderCheck.php">
-                <input type="submit" name="orderCheck" value="bestellen">
-            </form>
-
+<!--            <form class="margin-left" action="orderCheck.php">-->
+<!--                <input type="submit" name="orderCheck" value="bestellen">-->
+<!--            </form>-->
+<pre>
 <?php
 // sluit de for loop af
 }
-
+//    print_r($_SESSION["winkelwagen"]);
 ?>
+</pre>
+<script>
+    jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
+    jQuery('.quantity').each(function() {
+        var spinner = jQuery(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity-up'),
+            btnDown = spinner.find('.quantity-down'),
+            min = input.attr('min'),
+            max = input.attr('max');
 
+        btnUp.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+    });
+</script>
 </html>
