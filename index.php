42a5f0e39181613858e1fb39b337f1ca7f4df7a2
RickBlom <rickblom1980@gmail.com>
2019-12-18 21:45:08 +0100

    Namens Gerko: een home page

--- index.php
+++ index.php
@@ -30,4 +30,41 @@ if(isset($_GET['na'])){
     <div class="jumbotron"><h3 class="display-2">Welcome&nbsp;<?php if(isset($_SESSION['user_data'])){ print($_SESSION['user_data']['Firstname']);} ?></h3></div>
 </div>
 </body>
+<?php
+$link = mysqli_connect("localhost", "root", "", "wideworldimporters");
+
+// Check connection
+if ($link === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql= "SELECT i.StockItemName, i.RecommendedRetailPrice, i.MarketingComments, i.StockItemID
+FROM stockgroups s
+JOIN stockitemstockgroups g ON s.StockGroupID=g.StockGroupID
+JOIN stockitems i ON g.StockItemID=i.StockItemID
+JOIN stockitemholdings h ON i.StockItemID=h.StockItemID
+ORDER BY rand()
+LIMIT 3";
+
+
+
+$result = mysqli_query($link, $sql);
+if ($result->num_rows > 0) {
+    echo '<div class="container"><div class="row">';
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        $naam = $row["StockItemName"];
+        $prijs = $row["RecommendedRetailPrice"];
+        $omschrijvingg = $row["MarketingComments"];
+        $ID = $row["StockItemID"];
+        ?> <div class="col-sm-4">
+            <?php echo $naam . "<br>" . $omschrijvingg. "<br>";?>
+            <img src="images/img1.jpg"/" style="width:150px;height:150px;"><br>
+            <?php echo  "€" . $prijs; ?>
+            <div><a href="showitem.php?item_id=<?php echo $row["StockItemID"] ?>"
+                    class="btn btn-primary koop-knop">Go to Product</a>
+            </div>
+        </div>
+    <?php }} ?>
+    </div>
+    </div>
 <?php include_once "footer.php"; ?>
